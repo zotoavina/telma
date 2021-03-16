da3f20922756f28189ecc834a348c1957bfb5634
Zotoavina <zotoavinanantenaina@gmail.com>
2021-03-16 18:54:50 +0100

    fg

---
+++ src/test/java/com/mobile/telma/TelmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.telma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
