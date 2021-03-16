dfac92971ea5f20f8bcc706f8de7fade75355e3c
Zotoavina <zotoavinanantenaina@gmail.com>
2021-03-16 20:54:01 +0100

    d

--- src/main/java/com/mobile/telma/controllers/AdminController.java
+++ src/main/java/com/mobile/telma/controllers/AdminController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ public class AdminController {
 	@Autowired
 	AdminService adminService;
 	
+	@CrossOrigin(origins = "https://telmaproject.herokuapp.com/")
 	@PostMapping("")
 	public ResponseEntity<Map<String, Object>> login(@RequestBody Map<String, Object> adminMap) {
 		String email = (String) adminMap.get("email");
