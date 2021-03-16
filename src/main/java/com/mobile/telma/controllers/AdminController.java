974f2cb745b60a4be02121404276c8ee2790df41
Zotoavina <zotoavinanantenaina@gmail.com>
2021-03-16 20:41:24 +0100

    dg

--- src/main/java/com/mobile/telma/controllers/AdminController.java
+++ src/main/java/com/mobile/telma/controllers/AdminController.java
@@ -1,11 +1,18 @@
 package com.mobile.telma.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mobile.telma.domains.Admin;
 import com.mobile.telma.services.AdminService;
 
 @RestController
@@ -16,8 +23,15 @@ public class AdminController {
 	AdminService adminService;
 	
 	@PostMapping("")
-	public String login() {
-		return "login";
+	public ResponseEntity<Map<String, Object>> login(@RequestBody Map<String, Object> adminMap) {
+		String email = (String) adminMap.get("email");
+		String mdp = (String) adminMap.get("mdp");
+	    Admin admin = adminService.getByEmailAndMdp(email, mdp);
+	    Map<String, Object> map = new HashMap<>();
+	    map.put("status", HttpStatus.OK.value());
+	    map.put("message", "Identification de l' admin reussi");
+	    map.put("data", admin);
+	    return new ResponseEntity<>(map, HttpStatus.OK);
 	}
 	
 	@GetMapping("/")
